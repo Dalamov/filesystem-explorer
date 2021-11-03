92a67e8e0bf63721806f1bc247c1e9a39619ca69
AranBeitia <arantza.beitia@gmail.com>
2021-11-03 13:45:59 +0100

    datatable

--- index.php
+++ index.php
@@ -8,32 +8,58 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js" ></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
   <title>File System</title>
-  <script>
-    $(document).ready( function () {
-      $('#table_id').DataTable();
-  } );
-  </script>
+
 </head>
 <body>
-<table id="table_id" class="display">
-    <thead>
-        <tr>
-            <th>Column 1</th>
-            <th>Column 2</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Row 1 Data 1</td>
-            <td>Row 1 Data 2</td>
-        </tr>
-        <tr>
-            <td>Row 2 Data 1</td>
-            <td>Row 2 Data 2</td>
-        </tr>
-    </tbody>
-</table>
+  <header>header</header>
+  <main class="container">
+
+    <button class="btn btn-primary">add</button>
+    <table id="file-explorer" class="table table-primary table-bordered table-hover table-striped">
+        <thead>
+            <tr>
+                <th>Folder</th>
+                <th>File</th>
+                <th>Type</th>
+                <th>Download/edit/delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1 Data 1</td>
+                <td>Row 1 Data 2</td>
+                <td>Row 1 Data 2</td>
+                <td>Row 1 Data 2</td>
+            </tr>
+            <tr>
+                <td>Row 2 Data 1</td>
+                <td>Row 2 Data 2</td>
+                <td>Row 2 Data 2</td>
+                <td>Row 2 Data 2</td>
+            </tr>
+        </tbody>
+    </table>
+  </main>
+  <script>
+    $(document).ready( function () {
+      $('#file-explorer').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "order": [],
+        // "ajax": {
+        //   url: "get_files.php",
+        //   type: "POST",
+        // },
+        "columnsDefs":[{
+          "targets": [0, 3, 4],
+          "orderable": false
+        }]
+      });
+  } );
+  </script>
 </body>
 </html>
